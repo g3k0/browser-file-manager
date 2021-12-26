ebf2e278a6baacbd05e2a2cd1ce84916d77c1d38
Christian Palazzo <info@christianpalazzo.org>
2021-12-26 01:34:18 +0100

    get uri in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AppService } from './app.service';
 import { ContentTree } from './models/ContentTree';
 
@@ -12,14 +13,20 @@ export class AppComponent implements OnInit {
   
   constructor(
     private appService: AppService,
+    private router : Router,
   ) {
 
   }
 
   ngOnInit() {
-    
+    this.href = this.router.url;
+
+    console.log('----------------------------');
+    console.log(this.router.url);
+    console.log('-----------------------------');
   }
 
+  private href: string = '';
   public content: ContentTree = this.appService.getContentTree();
 
 }
