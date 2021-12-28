61baa124156d4d3ec2da9fcb27c7806b3e94a075
Christian Palazzo <info@christianpalazzo.org>
2021-12-28 22:15:02 +0100

    table sorting

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Content, ContentTree } from '../models/ContentTree';
 import { UtilsService } from '../services/utils.service';
+import {Sort} from '@angular/material/sort';
 
 @Component({
   selector: 'content-component',
@@ -22,12 +23,17 @@ export class ContentComponent implements OnInit {
     this.href = this.router.url.substring(1);
     this.contentTree = this.utilsService.getContent();
     this.folder =  this.getCurrentFolder();
+
+    if (this.folder[0].content) {
+      this.sortedData = this.folder[0].content.slice();
+    }
   }
 
   private href: string = '';
   private contentTree: ContentTree = { contentTree: [] }
   public folder: Content[]  = [];
   public columnsToDisplay = ['name', 'date-modified', 'type', 'size'];
+  public sortedData: Content[] = [];
 
   private getCurrentFolder(): Content[] {
 
@@ -58,6 +64,35 @@ export class ContentComponent implements OnInit {
       this.router.navigateByUrl(element.path);
     }
     return;
+  }
+
+  private compare(a: number | string, b: number | string, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+
+  sortData(sort: Sort): void {
+
+    if (this.folder[0].content) {
+      const data = this.folder[0].content.slice();
+      if (!sort.active || sort.direction === '') {
+        this.sortedData = data;
+        return;
+      }
+
+      this.sortedData = data.sort((a, b) => {
+        const isAsc = sort.direction === 'asc';
+        switch (sort.active) {
+          case 'name':
+            return this.compare(a.name, b.name, isAsc);
+          case 'date-modified':
+            return this.compare(a.modificationDate, b.modificationDate, isAsc);
+          case 'type':
+            return this.compare(a.type, b.type, isAsc);
+          default:
+            return 0;
+        }
+      });
+    }
   }
 
 }
