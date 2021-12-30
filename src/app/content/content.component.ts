75f24dd52b6e721c706bd168d59102755338168a
Christian Palazzo <info@christianpalazzo.org>
2021-12-30 20:21:37 +0100

    search logic

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -30,11 +30,12 @@ export class ContentComponent implements OnInit {
   }
 
   private href: string = '';
-  private contentTree: ContentTree = { contentTree: [] }
+  private contentTree: ContentTree = { contentTree: [] };
+  private backContent: Content[] = [];
   public folder: Content[]  = [];
   public columnsToDisplay = ['name', 'date-modified', 'type', 'size'];
   public sortedData: Content[] = [];
-  private backContent: Content[] = [];
+  public searchString: string = '';
 
   private getCurrentFolder(): Content[] {
 
@@ -63,7 +64,7 @@ export class ContentComponent implements OnInit {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
 
-  private loopForContent = (a: Content, b:Content[]) => {
+  private loopForBack = (a: Content, b:Content[]) => {
 
     if (a.content) {
       a.content.map((sc:Content) => {
@@ -72,12 +73,24 @@ export class ContentComponent implements OnInit {
           this.backContent = [a];
           return; 
         } else {
-          this.loopForContent(sc,b);
+          this.loopForBack(sc,b);
         }
         return sc;
       })
     }
   };
+
+  private loopForSearch = (stringToSearch: string, content:Content[], resultsContentArray: Content[]) => {
+    content.map((c:Content) => {
+      if (c.name.includes(stringToSearch)) {
+        resultsContentArray.push(c);
+      }
+      if (c.content) {
+        this.loopForSearch(stringToSearch, c.content, resultsContentArray);
+      }
+    });
+    return resultsContentArray;
+  }
 
   public doubleClick(element: Content): void {
     if (element.type === 'folder') {
@@ -118,7 +131,7 @@ export class ContentComponent implements OnInit {
    * 
    */
   public goBack(content: Content[]): void {
-    this.loopForContent(this.contentTree.contentTree[0], content);
+    this.loopForBack(this.contentTree.contentTree[0], content);
     this.folder = this.backContent;
     if (this.folder[0].content) {
       this.sortedData = this.folder[0].content.slice();
@@ -126,7 +139,23 @@ export class ContentComponent implements OnInit {
   }
 
   public search() {
-    console.log('search');
+    const searchResult: ContentTree = {
+      contentTree: [{
+        path: "",
+        modificationDate: "",
+        type: "folder",
+        name: "",
+        content: [],
+      }] 
+    };
+
+    const resultsArray = this.loopForSearch(this.searchString, this.contentTree.contentTree, []);
+    searchResult.contentTree[0].content = resultsArray;
+    this.folder = searchResult.contentTree;
+    if (this.folder[0].content) {
+      this.sortedData = this.folder[0].content.slice();
+    }
+
   }
 
 }
